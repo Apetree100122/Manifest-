098a57e335e3a0aea8299f4ca1de36b6d9086817
Alexander Petree <apetree1001@email.phoenix.edu>
2023-12-21 07:35:39 -0800

    Create Codeql_manifest.swift

---
+++ Codeql_manifest.swift
@@ -0,0 +1,30 @@
+---
+sourceLocationPrefix                     
+Users\runner\work\Manifest-\Manifest-base-Line
+<LinesOfCode: 0                            
+<unicodeNewlines: false
+<columnKind: utf8
+primaryLanguage: swift
+inProgress:
+  <primaryLanguage: swift
+  installedExtractors:
+  go:/Users/runner/hostedtoolcache/CodeQL/2.1/x64/code-ql/go
+ python:/Users/runner/hostedtoolcache/CodeQL/2.1/x64/codeql/python
+java: /Users/runner/hostedtoolcache/CodeQL/2.1/x64/codeql/java
+ html: /Users/runner/hostedtoolcache/CodeQL/2.1/x64/codeql/html
+ xml: /Users/runner/hostedtoolcache/CodeQL/2.1/x64/codeql/xml
+properties: /Users/runner/hostedtoolcache/CodeQL/2.1/x64/codeql/properties
+ cpp: /Users/runner/hostedtoolcache/CodeQL/2.1/x64/codeql/cpp
+swift: /Users/runner/hostedtoolcache/CodeQL/2.1/x64/codeql/swift
+csv:/Users/runner/hostedtoolcache/CodeQL/2.1/x64/codeql/csv
+ yaml:/Users/runner/hostedtoolcache/CodeQL/2.1/x64/codeql/yaml
+csharp: /Users/runner/hostedtoolcache/CodeQL/2.1/x64/codeql/csharp
+javascript: /Users/runner/hostedtoolcache/CodeQL/2.1/x64/codeql/javascript
+    ruby: / Users/runner/hostedtool 
+    cache/CodeQL/2.1/x64/codeql/ruby
+creationMetadata
+build: 7
+cliVersion: 3
+creationTime: 23-12-13
+finalized: true
+---
